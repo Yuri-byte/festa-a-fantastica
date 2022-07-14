0aaa7c3c622475d909ccdde2169726e8ec0ae7d6
Yuri-byte <yurischwab00@gmail.com>
2022-07-14 19:27:08 -0300

    photos of me

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CV from '../../assets/2.png'
+import CV from '../../assets/cv.pdf'
 
 const CTA = () => {
   return (
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/macaco_preto_e_branco.png'
+import ME from '../../assets/me.png'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
