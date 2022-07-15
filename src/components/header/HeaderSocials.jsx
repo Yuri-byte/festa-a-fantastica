56e84e171b2b7a9d5feb60178135d57e06b409d4
Yuri-byte <yurischwab00@gmail.com>
2022-07-14 23:28:28 -0300

    add link to socials

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -4,9 +4,8 @@ import {BsLinkedin, BsGithub, /* BsDribbble */} from 'react-icons/bs'
 function HeaderSocials() {
   return (
     <div className='header__socials'>
-      <a href='https://linkedin.com' target="_blank"><BsLinkedin/></a>
-      <a href='https://github.com' target="_blank"><BsGithub/></a>
-      {/* <a href='https://dribbble.com' target="_blank"><BsDribbble/></a> */}
+      <a href='https://www.linkedin.com/in/yurischwab/' target="_blank"><BsLinkedin/></a>
+      <a href='https://github.com/Yuri-byte' target="_blank"><BsGithub/></a>
     </div>
   )
 }
